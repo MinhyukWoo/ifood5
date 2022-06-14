c0c7057f86b28d1e9438027a95ae1cfc0ccf1d92
MinhyukWoo <minhyukwoo.dev@gmail.com>
2022-06-14 17:24:24 +0900

    Remove convert.py

--- sources/ifood/convert.py
+++
@@ -1,9 +0,0 @@
-from PIL.Image import Image
-from numpy import ndarray
-import numpy as np
-
-
-def img2ndarray(img: Image, size: (int, int)) -> ndarray:
-    img = img.convert("RGB").resize(size)
-    out = np.asarray(img, dtype=float)
-    return out.transpose((2, 0, 1))
